3bd6a82c37abdbe61078004e673f204195ec07a9
Umaima Zulfiqar <umaima749zulfiqar@gmail.com>
2025-12-09 21:06:12 +0500

    adding callback function to useState function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,54 @@
 import Header from "./components/Header"  
 import Main from "./components/Main"
+import React from "react" 
 
 export default function App() {
+  const [count, setCount] = React.useState(0)
+    
+     {/* 
+     * Note: if you ever need the old value of state
+     * to help you determine the new value of state,
+     * you should pass a callback function to your
+     * state setter function instead of using
+     * state directly. This callback function will
+     * receive the old value of state as its parameter,
+     * which you can then use to determine your new
+     * value of state. 
+     * 
+     * function add() {
+        setCount(prevCount=> prevCount + 1)
+        setCount(prevCount=> prevCount + 1)
+        setCount(prevCount=> prevCount + 1)
+      }
+
+      function subtract() {
+          setCount(count - 1)
+          setCount(count - 1)
+          setCount(count - 1)
+      }
+     * 
+     * */}
+     
+    function add() {
+        setCount(prevCount=> prevCount + 1)
+    }
+
+    function subtract() {
+        setCount(count - 1)
+    }
+
   return (
     <>  
       <Header />
       <Main />
+      <main className="container">
+            <h1>How many times will Bob say "state" in this section?</h1>
+            <div className="counter">
+                <button className="minus" onClick={subtract} aria-label="Decrease count">–</button>
+                <h2 className="count">{count}</h2>
+                <button className="plus" onClick={add} aria-label="Increase count">+</button>
+            </div>
+        </main>
     </>
   )
 }
