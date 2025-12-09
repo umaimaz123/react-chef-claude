b91ff9fc30bae8e8fc6201ce48aba046d76a650d
Umaima Zulfiqar <umaima749zulfiqar@gmail.com>
2025-12-09 19:44:44 +0500

    Added event lister onSubmit, used map to traverse ingredients array, added new ingredient to the array

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,21 @@
 export default function Main() {
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    
+    const ingredientsListItems = ingredients.map(ingredient => (
+      <li key={ingredient}>{ingredient}</li> // added key prop, and map all the way to return list of ingredients
+    ))
+
+    function handleOnSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        ingredients.push(newIngredient) // added new ingredient to the array
+        console.log(ingredients) // log updated ingredients array
+    }
+    
   return (
       <main>
-        <form className="add-ingredient-form">
+        <form onSubmit={handleOnSubmit} className="add-ingredient-form">
           <input 
             type="text" 
             aria-label="Add ingredient" 
@@ -10,6 +24,9 @@ export default function Main() {
           />
           <button>Add ingredient</button>
         </form>
+        <ul>
+          {ingredientsListItems}  {/* rendered the list of ingredients through map */}
+        </ul>
       </main>
   )
 }
