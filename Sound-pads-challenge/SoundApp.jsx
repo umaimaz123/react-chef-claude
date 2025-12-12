8a5cc40837b3d10d671030b6b60295ec07274c67
Umaima Zulfiqar <umaima749zulfiqar@gmail.com>
2025-12-12 19:05:58 +0500

    added inline styling for sound pad buttons

--- Sound-pads-challenge/SoundApp.jsx
+++ Sound-pads-challenge/SoundApp.jsx
@@ -5,9 +5,15 @@ import "./soundPad.css"
 
 export default function SoundApp(props) {
   const [soundPads, setSoundPads] = React.useState(pads)
+
+  const styles = {
+    backgroundColor: props.darkMode ? "#222222" : "#cccccc",
+  }
+  
   const soundPadButtons = soundPads.map(pad => (
-      <button key={pad.id}></button>
+      <button style={styles} key={pad.id}></button>
   ))
+  
   return (
     <main>
       <div className="pad-container">
--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,4 @@ import ReactDOM from 'react-dom/client';
 import App from "./App"
 import SoundApp from "/Sound-pads-challenge/SoundApp.jsx"
 
-ReactDOM.createRoot(document.getElementById('root')).render(<SoundApp />);
+ReactDOM.createRoot(document.getElementById('root')).render(<SoundApp darkMode={false} />);
