6301283a3a5ece19759fe5a2ffe61af69cf840b2
Umaima Zulfiqar <umaima749zulfiqar@gmail.com>
2025-12-12 15:58:34 +0500

    passed state as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Count from "./components/Count"
 import Header from "./components/Header"  
 import Main from "./components/Main"
 import React from "react" 
 
 export default function App() {
   // ********* Commented out counter code **********
-  // const [count, setCount] = React.useState(0)
+  const [count, setCount] = React.useState(0)
     
      {/* 
      * Note: if you ever need the old value of state
@@ -30,27 +31,41 @@ export default function App() {
      * 
      * */}
      
-    // function add() {
-    //   setCount(prevCount=> prevCount + 1)
-    // }
+    function add() {
+      setCount(prevCount=> prevCount + 1)
+    }
 
-    // function subtract() {
-    //   setCount(count - 1)
-    // }
+    function subtract() {
+      setCount(count - 1)
+    }
 
   return (
     <>  
       <Header />
       <Main />
 
-      {/* <main className="container">
+      <main className="container">
         <h1>How many times will Bob say "state" in this section?</h1>
         <div className="counter">
           <button className="minus" onClick={subtract} aria-label="Decrease count">–</button>
-          <h2 className="count">{count}</h2>
+          <Count number={count} />
           <button className="plus" onClick={add} aria-label="Increase count">+</button>
         </div>
-      </main> */}
+      </main>
+
+       {/* /**
+        * Passing state as props Challenge:
+        * - Create a new component called `Count`
+        *    - It should receive a prop called `number`, whose value
+        *      is the current value of our count
+        *    - Have the component render the h2.count element below
+        *      and display the incoming prop `number`
+        * - Replace the h2.count below with an instance of
+        *   the new Count component, passing the correct value
+        *   to its `number` prop.
+        * - After doing this, everything should be working the
+        *   same as before.
+        */}
     </>
   )
 }
